52a34da18c1022767b52f1ae07684fc3ea535630
nadian-melhem <nadian.melhem@gmail.com>
2021-04-21 16:14:31 +0300

    hi

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:movie_app/components/genre_card.dart';
 import 'package:movie_app/constants.dart';
-import 'package:movie_app/models/movie.dart';
-import 'dart:math' as math;
-
 import 'categories.dart';
 import 'genres.dart';
-import 'movie_card.dart';
 import 'movie_carousel.dart';
 
 class Body extends StatelessWidget {
